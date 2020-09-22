5e30d943ad182219ebed64187a54c145e816fef1
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-09-22 15:41:13 -0300

    Adicionando rotas

--- frontend/lib/app/routes/app_pages.dart
+++ frontend/lib/app/routes/app_pages.dart
@@ -1,10 +1,12 @@
 import 'package:frontend/app/modules/home/home_page.dart';
+import 'package:frontend/app/modules/splash/splash_page.dart';
 import 'package:get/get.dart';
 part './app_routes.dart';
 
 abstract class AppPages {
 
   static final pages = [
+    GetPage(name: Routes.INITIAL, page:()=> SplashPage(),),
     GetPage(name: Routes.HOME, page:()=> HomePage(),)
   ];
 }
--- frontend/lib/app/routes/app_routes.dart
+++ frontend/lib/app/routes/app_routes.dart
@@ -1,7 +1,7 @@
 part of './app_pages.dart';
 abstract class Routes{
 
-  static const INITIAL = '/';
+  static const INITIAL = '/splash';
   static const HOME = '/home';
   static const LOGIN = '/login';
   static const CADASTRO = '/cadastro';
