e1b9fd6419f568e091004d24841f96a071ee44c4
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-09-22 17:34:36 -0300

    criando nova rota

--- frontend/lib/app/routes/app_pages.dart
+++ frontend/lib/app/routes/app_pages.dart
@@ -1,4 +1,5 @@
-import 'package:frontend/app/modules/home/home_page.dart';
+import 'package:frontend/app/modules/home/desktop/homedesktop_page.dart';
+import 'package:frontend/app/modules/home/web/homeweb_page.dart';
 import 'package:frontend/app/modules/splash/splash_page.dart';
 import 'package:get/get.dart';
 part './app_routes.dart';
@@ -7,6 +8,7 @@ abstract class AppPages {
 
   static final pages = [
     GetPage(name: Routes.INITIAL, page:()=> SplashPage(),),
-    GetPage(name: Routes.HOME, page:()=> HomePage(),)
+    GetPage(name: Routes.HOME_DESKTOP, page:()=> HomeDesktopPage(),),
+    GetPage(name: Routes.HOME_WEB, page:()=> HomeWebPage(),),
   ];
 }
--- frontend/lib/app/routes/app_routes.dart
+++ frontend/lib/app/routes/app_routes.dart
@@ -2,7 +2,8 @@ part of './app_pages.dart';
 abstract class Routes{
 
   static const INITIAL = '/splash';
-  static const HOME = '/home';
+  static const HOME_DESKTOP = '/home_desktop';
+  static const HOME_WEB = '/home_web';
   static const LOGIN = '/login';
   static const CADASTRO = '/cadastro';
 }
