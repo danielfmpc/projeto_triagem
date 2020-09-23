d89e7d7f51bceebba2de172ed8b1caf65d195e09
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-09-23 10:25:00 -0300

    enviando evento para o servidor

--- frontend/lib/app/modules/home/web/homeweb_page.dart
+++ frontend/lib/app/modules/home/web/homeweb_page.dart
@@ -9,24 +9,24 @@ class HomeWebPage extends StatefulWidget {
 
 
 class _HomeWebPageState extends State<HomeWebPage> {
-  String _users = "";
+  IO.Socket socket;
   @override
   void initState() {
 
     // Dart client
-    IO.Socket socket = IO.io('http://192.168.20.7:3000', <String, dynamic>{
+    socket = IO.io('http://192.168.20.7:3000', <String, dynamic>{
       'transports': ['websocket'],
     });
     // socket.on('connection', (_) {
     //  print('connect');
     //  socket.emit('msg', 'test');
     // });
-    socket.on('greeting', (data) {
-      print(data);
-      setState(() {
-        _users += " "+data['user'];
-      });
-    });
+    // socket.on('greeting', (data) {
+    //   print(data);
+    //   setState(() {
+    //     _users += " "+data['user'];
+    //   });
+    // });
     socket.on('disconnect', (_) => print('disconnect'));
     socket.connect();
     super.initState();
@@ -42,11 +42,12 @@ class _HomeWebPageState extends State<HomeWebPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('List Users'),
-            Text(
-              '$_users',
-              style: Theme.of(context).textTheme.headline4,
-            )
+            RaisedButton(
+              child: Text('Enviar'),
+              onPressed: (){
+                socket.emit('greeting', "teste");
+              },
+            ),
           ],
         ),
       ),
