bd3cc3395cf738015580cf6700df50406dbc9865
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-09-28 01:17:05 -0300

    adicionando som ao chamar senha

--- frontend/lib/app/modules/home/desktop/homedesktop_page.dart
+++ frontend/lib/app/modules/home/desktop/homedesktop_page.dart
@@ -1,10 +1,9 @@
 
-import 'package:audioplayers/audio_cache.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_audio_desktop/flutter_audio_desktop.dart';
 import 'package:frontend/app/utils/window_size.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+
 
 class HomeDesktopPage extends StatefulWidget {
   @override
@@ -15,18 +14,14 @@ class _HomeDesktopPageState extends State<HomeDesktopPage> {
   IO.Socket socket;
   String _triagem = "";
   String _status = "";
-  AudioCache audioCache;
-  AudioPlayer audioPlayer = AudioPlayer();
-
-  
-
+  AudioPlayer audioPlayer = new AudioPlayer(debug: true);
+  String _textFieldValue = '';
 
   @override
-  void initState() {
-    audioCache = AudioCache();
-
+  void initState() {    
     WindowSizeService().initialize();
-
+    _textFieldValue = 'F:/flutter/projeto_triagem/frontend/assets/audios/chamada.mp3';
+    
     // Dart client
     print("[webscoket][iniciado]");
     socket = IO.io('http://192.168.20.237:3000', <String, dynamic>{
@@ -37,52 +32,37 @@ class _HomeDesktopPageState extends State<HomeDesktopPage> {
       setState(() {
         _triagem = data['triagem'];
         _status = data['status'];
+        initializePlay();
       });
     });
     socket.on('[webscoket][disconnect]', (_) => print('disconnect'));
     print("[webscoket][conectado]");
     socket.connect();
-   
     super.initState();
   }
 
-  playAudioFromLocalStorage(path) async {
-    audioPlayer = await audioCache.play(path);
-    // if (audioPlayer == 1) {
-    //   // success
-
-    // } else {
-    //   print('Some error occured in playing from storage!');
-    // }
-  }
-  pauseAudio() async {
-    int response = await audioPlayer.pause();
-    if (response == 1) {
-      // success
-
+  void initializePlay() async {
+    bool result =  await audioPlayer.load(this._textFieldValue);
+    if(result){
+      print('loaded');
+      this.setState(() {});
+      if (!audioPlayer.isLoaded) {
+        print('Load audio first.');
+      } else {
+        if (audioPlayer.isPaused) {
+          /// Playing loaded audio file.
+          this.audioPlayer.play();
+        } else if (audioPlayer.isPlaying) {
+          /// Pausing playback of loaded audio file.
+          this.audioPlayer.pause();
+        }
+      }
     } else {
-      print('Some error occured in pausing');
+      print('not loaded');
     }
-  }
-  stopAudio() async {
-    int response = await audioPlayer.stop();
-    if (response == 1) {
-      // success
 
-    } else {
-      print('Some error occured in stopping');
-    }
   }
-  resumeAudio() async {
-    int response = await audioPlayer.resume();
-    if (response == 1) {
-      // success
-
-    } else {
-      print('Some error occured in resuming');
-    }
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -115,15 +95,7 @@ class _HomeDesktopPageState extends State<HomeDesktopPage> {
       ),
       body: Center(
         child: Column(
-          children: [    
-            FlatButton(
-              onPressed: ()async{
-                var path = "assets/audios/chamada.mp3";
-                playAudioFromLocalStorage(path);
-                 
-              },
-              child: Text('Tocar'),
-            ),        
+          children: [   
             Padding(
               padding: const EdgeInsets.only(top: 29, left: 8, right: 8),
               child: Container(
@@ -145,9 +117,7 @@ class _HomeDesktopPageState extends State<HomeDesktopPage> {
                   ),
                 ),
               ),
-            ),
-            
-            
+            ),           
             Padding(
               padding: const EdgeInsets.only(top: 26, left: 8, right: 8),
               child: Container(
