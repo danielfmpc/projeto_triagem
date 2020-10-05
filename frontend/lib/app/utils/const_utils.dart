6bc5aad7f02578c36efab3fe75854c52fab88539
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-10-05 16:50:44 -0300

    adicionando url base

--- frontend/lib/app/utils/const_utils.dart
+++ frontend/lib/app/utils/const_utils.dart
@@ -1,5 +1,5 @@
 abstract class ConstUtils {
-  static const BASE_URL_WS = 'ws://localhost:6969';
+  static const BASE_URL_WS = 'http://localhost:3000';
   static const APP_KEY = 'c61586499076687b027e';
   static const PUSHER_CLUSTER = 'us2';
 }
